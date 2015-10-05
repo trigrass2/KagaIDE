f5f460dc5de48a2b0c2d276f7a93a9a2f1ebb8b7
jiajidi <jiajidi@163.com>
2015-10-05 13:42:28 +0800

    符号表管理器

---
+++ KageIDE/KageIDE/Module/SymbolManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KageIDE.Struct;
+using KageIDE.Enuming;
+
+namespace KageIDE.Module
+{
+    public class SymbolManager
+    {
+        // 工厂
+        public static SymbolManager getInstance()
+        {
+            return synObject == null ? synObject = new SymbolManager() : synObject;
+        }
+        
+        // 清空符号表
+        public void clear()
+        {
+            tableContainer.Clear();
+            callfunContainer.Clear();
+        }
+
+        // 符号表向量
+        public List<KagaTable> tableContainer = null;
+
+        // 函数名向量
+        public List<string> callfunContainer = null;
+
+        // 私有的构造器
+        private SymbolManager()
+        {
+            tableContainer = new List<KagaTable>();
+            callfunContainer = new List<string>();
+        }
+        // 唯一实例
+        private static SymbolManager synObject = null;
+    }
+}
