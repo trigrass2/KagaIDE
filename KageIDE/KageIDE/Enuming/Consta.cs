51d00fd7750627e4b2011bd2a7d470e3cf286e92
jiajidi <jiajidi@163.com>
2015-10-06 01:46:08 +0800

    改用GridDataView管理增删列表

---
+++ KageIDE/KageIDE/Enuming/Consta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KagaIDE.Enuming
+{
+    // 管理全局常量的类
+    public static class Consta
+    {
+        // 基础数据类型
+        public static List<string> basicType = new List<string>()
+        {
+            "int",
+            "char",
+            "long",
+            "float",
+            "double",
+            "unsigned int",
+            "unsigned char"
+        };
+
+        // 基础返回类型
+        public static List<string> returnType = new List<string>()
+        {
+            "void",
+            "int",
+            "long",
+            "char",
+            "float",
+            "double",
+            "unsigned int",
+            "unsigned char"
+        };
+    }
+}
