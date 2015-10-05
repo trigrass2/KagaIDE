ffd24196f4d1a24398a26ef0dd2f3daa471d7a4f
jiajidi <jiajidi@163.com>
2015-10-05 11:13:33 +0800

    枚举

---
+++ KageIDE/KageIDE/Enuming/NodeType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KageIDE.Enuming
+{
+    public enum NodeType
+    {
+        // 空操作
+        NOP,
+        // 注释
+        NOTE,
+        // 开关定义
+        DEFINE_SWITCHES,
+        // 变量定义
+        DEFINE_VARIABLE,
+        // 表达式
+        EXPRESSION,
+        // 条件语句
+        IF,
+        // 分支语句
+        IF_ELSE,
+        // 条件循环
+        WHILE,
+        // 后置条件循环
+        DO_WHILE,
+        // 边界循环
+        FOR,
+        // 遍历循环
+        FOREACH,
+        // 中断循环
+        BREAK,
+        // 返回
+        RETURN,
+        // 函数调用
+        CALL,
+        // 单次函数调用
+        RUNONCE,
+        // 代码片段
+        CODEBLOCK
+    }
+}
---
+++ KageIDE/KageIDE/KagaNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KageIDE.Enuming;
+
+namespace KageIDE
+{
+    public class KagaNode
+    {
+        // 构造器
+        public KagaNode() { }
+
+
+
+        // 节点深度
+        int depth = 0;
+        // 节点类型
+        NodeType type = NodeType.NOP;
+        // 特殊尾部节点（括号）标记
+        bool isBrucketNode = false;
+        // 空节点标记位
+        bool isNilNode = false;
+        // 多路子树
+        public List<KagaNode> subNode = null;
+    }
+}
