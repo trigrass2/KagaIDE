a94d353a4f1f04f2b7d1d473ccd826f64b426629
jiajidi <jiajidi@163.com>
2015-10-05 12:12:04 +0800

    符号表

---
+++ KageIDE/KageIDE/Struct/KagaTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KageIDE.Struct
+{
+    public class KagaTable
+    {
+        // 构造器
+        public KagaTable(int tableDepth)
+        {
+            this.symbols = new List<string>();
+            this.prefix = String.Format("___KAGA_VAR_{0}", tableDepth);
+        }
+
+        // 编译模式取得所有符号
+        public List<string> getParseTable()
+        {
+            List<string> parList = new List<string>();
+            foreach (string s in symbols)
+            {
+                parList.Add(String.Format("{0}_{1}___", this.prefix, s));
+            }
+            return parList;
+        }
+
+        // 符号表数量
+        public int size()
+        {
+            return this.symbols.Count;
+        }
+
+        // 符号表前缀
+        public string prefix = "___KAGA_";
+
+        // 符号列表
+        public List<string> symbols = null;
+    }
+}
