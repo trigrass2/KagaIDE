93c2e54bd0ccf514429a4306804e75ae7edc8b90
jiajidi@163.com <jiajidi@163.com>
2015-10-06 20:21:49 +0800

    函数基本类型

--- KageIDE/KageIDE/Struct/FunctionCell.cs
+++ KageIDE/KageIDE/Struct/FunctionCell.cs
@@ -1,19 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using KagaIDE.Struct;
 
 namespace KagaIDE.Enuming
 {
     public class FunctionCell
     {
         // 构造器
-        public FunctionCell()
+        public FunctionCell(
+            string fname,
+            List<KagaVar> args = null,
+            VarType rt = VarType.VOID)
         {
+            this.callname = fname;
+            this.returnType = rt;
+            this.paraList = args;
+        }
 
+        // 获得编译时函数签名
+        public string getSign(bool containLeftBrucket = false)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.returnType.ToString().ToLower().Replace('_', ' '));
+            sb.Append(" ");
+            sb.Append(this.callname);
+            sb.Append("(");
+            for (int i = 0; i < this.paraList.Count; i++)
+            {
+                sb.Append(String.Format(
+                    "{0} {1}", this.paraList[i].vartype, this.paraList[i].varname));
+                if (i != this.paraList.Count - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+            sb.Append(")");
+            if (containLeftBrucket == true)
+            {
+                sb.Append(" {");
+            }
+            return sb.ToString();
         }
 
         // 函数头
         public string callname = "___KAGA_";
-        // 函数列表
+        // 返回类型
+        public VarType returnType = VarType.VOID;
+        // 参数列表
+        public List<KagaVar> paraList = new List<KagaVar>();
+
     }
 }
--- KageIDE/KageIDE/Struct/KagaTable.cs
+++ KageIDE/KageIDE/Struct/KagaTable.cs
@@ -10,7 +10,7 @@ namespace KagaIDE.Struct
         public KagaTable(int tableDepth)
         {
             this.depth = tableDepth;
-            this.symbols = new List<string>();
+            this.symbols = new List<KagaVar>();
             this.prefix = String.Format("___KAGA_VAR_{0}", this.depth);
         }
 
@@ -18,9 +18,9 @@ namespace KagaIDE.Struct
         public List<string> getParseTable()
         {
             List<string> parList = new List<string>();
-            foreach (string s in symbols)
+            foreach (KagaVar s in symbols)
             {
-                parList.Add(String.Format("{0}_{1}___", this.prefix, s));
+                parList.Add(String.Format("{0}_{1}@{2}", this.prefix, s.varname, s.vartype));
             }
             return parList;
         }
@@ -36,6 +36,6 @@ namespace KagaIDE.Struct
         // 符号表前缀
         public string prefix = "___KAGA_";
         // 符号列表
-        public List<string> symbols = null;
+        public List<KagaVar> symbols = null;
     }
 }
