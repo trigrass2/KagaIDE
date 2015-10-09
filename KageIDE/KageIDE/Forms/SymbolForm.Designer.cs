0c4eac3e9a32b3fb6dd9574f52c7abc75f338515
jiajidi <jiajidi@163.com>
2015-10-09 13:23:49 +0800

    符号管理器可视化、关于窗口

---
+++ KageIDE/KageIDE/Forms/SymbolForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace KagaIDE.Forms
+{
+    partial class SymbolForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SymbolForm));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.funListBox = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(0, 0);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(579, 367);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.funListBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(571, 341);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "函数管理";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(571, 341);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "变量管理";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // funListBox
+            // 
+            this.funListBox.FormattingEnabled = true;
+            this.funListBox.ItemHeight = 12;
+            this.funListBox.Location = new System.Drawing.Point(5, 7);
+            this.funListBox.Name = "funListBox";
+            this.funListBox.Size = new System.Drawing.Size(150, 328);
+            this.funListBox.TabIndex = 0;
+            this.funListBox.SelectedIndexChanged += new System.EventHandler(this.funListBox_SelectedIndexChanged);
+            // 
+            // SymbolForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 365);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SymbolForm";
+            this.Text = "符号管理器";
+            this.Load += new System.EventHandler(this.SymbolForm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        public System.Windows.Forms.ListBox funListBox;
+    }
+}
---
+++ KageIDE/KageIDE/Forms/SymbolForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using KagaIDE.Module;
+
+namespace KagaIDE.Forms
+{
+    public partial class SymbolForm : Form
+    {
+        // 内核的唯一实例
+        private KagaController core = KagaController.getInstance();
+
+        public SymbolForm(int pageStop)
+        {
+            InitializeComponent();
+            // 选中默认页面
+            this.tabControl1.SelectedTab = this.tabControl1.TabPages[pageStop];
+        }
+
+        private void SymbolForm_Load(object sender, EventArgs e)
+        {
+            // 加载函数管理控件页面
+            FunctionForm funform = new FunctionForm("管理函数");
+            funform.TopLevel = false;
+            funform.Location = new Point(165, 0);
+            funform.Size = new System.Drawing.Size(500, 500);
+            funform.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+            funform.FormBorderStyle = FormBorderStyle.None;
+            funform.Show();
+            funform.Name = "FunForm";
+            funform.setMainFormPointer((MainForm)this.Owner, this);
+            this.tabControl1.TabPages[0].Controls.Add(funform);
+            // 加载函数清单
+            List<string> funList = core.getAllFunction();
+            foreach (string s in funList)
+            {
+                this.funListBox.Items.Add(s);
+            }
+            this.funListBox.SelectedIndex = 0;
+        }
+
+        private void funListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ((FunctionForm)this.tabControl1.TabPages[0].Controls["FunForm"]).refreshContext(
+                (string)this.funListBox.SelectedItem);
+            this.tabControl1.TabPages[0].Controls["FunForm"].Enabled = true;
+            // main函数不可编辑
+            if ((string)this.funListBox.Items[this.funListBox.SelectedIndex] == "main")
+            {
+                this.tabControl1.TabPages[0].Controls["FunForm"].Enabled = false;
+            }
+        }
+    }
+}
